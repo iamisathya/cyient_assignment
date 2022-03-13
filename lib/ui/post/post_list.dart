2bc48ab8982e359752b7e6af5dca73235220c8f3
Sathya <sathyanarayana.an@unicity.com>
2022-03-14 01:08:00 +0530

    Showing progress for intial fetching

--- lib/ui/post/post_list.dart
+++ lib/ui/post/post_list.dart
@@ -6,7 +6,6 @@ import 'package:provider/provider.dart';
 
 import 'widgets/post_item.dart';
 
-
 class PostListWidget extends StatelessWidget {
   final List<Post> _data;
   bool _isLoading;
@@ -22,30 +21,40 @@ class PostListWidget extends StatelessWidget {
   }
 
   Widget _scrollNotificationWidget() {
-    return Column(
+    return Stack(
       children: [
-        Expanded(
-          child: NotificationListener<ScrollNotification>(
-            onNotification: _scrollNotification,
-            child: ListView.builder(
-              itemCount: _data.length,
-              itemBuilder: (context, index) {
-                return PostItem(data: _data, index: index);
-              },
-            ),
-          ),
+        if (_dataState == DataState.INITIAL_FETCHING) // showing progress for initial fetching
+        const Positioned(
+          child: Align(
+              alignment: Alignment.center, child: CircularProgressIndicator()),
         ),
-        if (_dataState == DataState.MORE_FETCHING)
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: const [
-                CircularProgressIndicator(),
-                SizedBox(height: 8),
-                Center(child: Text("Showing catched data with pagintation")),
-              ],
+        Column(
+          children: [
+            Expanded(
+              child: NotificationListener<ScrollNotification>(
+                onNotification: _scrollNotification,
+                child: ListView.builder(
+                  itemCount: _data.length,
+                  itemBuilder: (context, index) {
+                    return PostItem(data: _data, index: index);
+                  },
+                ),
+              ),
             ),
-          ),
+            if (_dataState == DataState.MORE_FETCHING)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: const [
+                    CircularProgressIndicator(),
+                    SizedBox(height: 8),
+                    Center(
+                        child: Text("Showing catched data with pagintation")),
+                  ],
+                ),
+              ),
+          ],
+        ),
       ],
     );
   }
