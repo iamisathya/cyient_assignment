91827e1a829e9b98e80aec4e076c62815f94950c
Sathya <sathyanarayana.an@unicity.com>
2022-03-14 03:39:04 +0530

    minor changes

--- lib/ui/users/users.dart
+++ lib/ui/users/users.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:cyient_assignment/models/user/user.dart';
 import 'package:cyient_assignment/stores/post/post_store.dart';
 import 'package:flutter/material.dart';
